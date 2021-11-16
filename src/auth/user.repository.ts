48d13c5dd33f0dff010f2e7e3b7e42bbff98079f
onesmus muna <onesmusjones@gmail.com>
2021-11-16 12:07:25 +0300

    hashing passwords

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -3,6 +3,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { genSalt, hash } from 'bcrypt';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
@@ -13,7 +14,8 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await genSalt();
+    user.password = await this.hashPassword(password, user.salt);
 
     try {
       await user.save();
@@ -25,5 +27,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return hash(password, salt);
   }
 }
